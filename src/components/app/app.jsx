cb4b895ac63ea0643b1f3ed92629c91ae9e71d0a
Вини <yvinivitin@gmail.com>
2021-02-17 14:08:17 +0200

    Добавлены маршруты

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import MainScreen from '../main-screen/main-screen.jsx';
+import Login from '../login/login.jsx';
+import Favorites from '../favorites/favorites.jsx';
+import Room from '../room/room.jsx';
+import NotFoundScreen from '../not-found-screen/not-found-screen.jsx';
 
 const App = (props) => {
+  const {cardsCount} = props;
+
   return (
-    <MainScreen cardsCount={props.cardsCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainScreen cardsCount={cardsCount} />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+        <Route exact path="/offer/:id">
+          <Room />
+        </Route>
+        <Route>
+          <NotFoundScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
